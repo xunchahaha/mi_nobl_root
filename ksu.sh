7528acd4b353436352274d4f5808791d6229e320
Skorpion96 <39702358+Skorpion96@users.noreply.github.com>
2026-03-10 20:51:54 +0100

    Modify ksu.sh to enhance loading messages and flow
    Updated ksu.sh to include loading message and sleep before executing fix_lspd.sh.

--- ksu.sh
+++ ksu.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-/system/bin/service call miui.mqsas.IMQSNative 21 i32 1 s16 "sh" i32 1 s16 "/data/local/tmp/ksu_step1.sh" s16 "/storage/emulated/0/ksu_result.txt" i32 60 >nul 2>&1
-/system/bin/service call miui.mqsas.IMQSNative 21 i32 1 s16 "sh" i32 1 s16 "/data/local/tmp/ksu_step2.sh" s16 "/storage/emulated/0/ksu_result.txt" i32 60 >nul 2>&1
-/system/bin/cat /storage/emulated/0/ksu_result.txt 2>nul | findstr "ALL_DONE" >nul 2>&1 || echo "ksu loaded"
+/system/bin/service call miui.mqsas.IMQSNative 21 i32 1 s16 "sh" i32 1 s16 "/data/local/tmp/ksu_step1.sh" s16 "/data/local/tmp/ksu_result.txt" i32 60 >nul 2>&1
+/system/bin/service call miui.mqsas.IMQSNative 21 i32 1 s16 "sh" i32 1 s16 "/data/local/tmp/ksu_step2.sh" s16 "/data/local/tmp/ksu_result.txt" i32 60 >nul 2>&1
+/system/bin/cat /storage/emulated/0/ksu_result.txt 2>nul | findstr "ALL_DONE" >nul 2>&1 || echo "ksu loaded, loading lspd"
+sleep 3
+/system/bin/service call miui.mqsas.IMQSNative 21 i32 1 s16 '/system/bin/sh' i32 1 s16 '/data/local/tmp/fix_lspd.sh' s16 '/data/local/tmp/lspd_fix_out.txt' i32 180
